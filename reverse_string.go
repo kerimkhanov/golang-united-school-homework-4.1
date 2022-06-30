b855e10f4d8ae84998b9904b86f1ee018aba1016
english2two <english2two@gmail.com>
2022-06-30 18:05:35 +0600

    version 1.0

--- reverse_string.go
+++ reverse_string.go
@@ -2,5 +2,14 @@ package reverse_string
 
 func ReverseString(input string) (output string) {
 	// solution goes here
+	rArr := []rune(input)
+	i := 0
+	j := len(rArr) - 1
+	for i < j {
+		rArr[i], rArr[j] = rArr[j], rArr[i]
+		i++
+		j--
+	}
+	output = string(rArr)
 	return output
 }
